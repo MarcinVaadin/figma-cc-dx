4833c16220f0b76b858f05fada0cc408a9baccf3
marcin <marcin@vaadin.com>
2025-07-31 14:07:43 +0200

    AcmeCard api

--- src/main/java/com/example/application/components/AcmeCard.java
+++ src/main/java/com/example/application/components/AcmeCard.java
@@ -1,6 +1,34 @@
 package com.example.application.components;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.card.Card;
 
+import java.util.Collection;
+
 public class AcmeCard extends Card {
+
+    @Override
+    public void setTitle(String title) {
+        super.setTitle(title);
+    }
+
+    @Override
+    public Component getTitle() {
+        return super.getTitle();
+    }
+
+    @Override
+    public void setSubtitle(Component subtitle) {
+        super.setSubtitle(subtitle);
+    }
+
+    @Override
+    public Component getSubtitle() {
+        return super.getSubtitle();
+    }
+
+    @Override
+    public void add(Collection<Component> components) {
+        super.add(components);
+    }
 }
